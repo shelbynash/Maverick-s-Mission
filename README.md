d58dc921bf9b4d037d3a766d8ee0abf1f5b5ceaa
Shelby Nash <shelbylnash@gmail.com>
2022-11-18 00:16:37 -0500

    Cleaned up the codeand connected Javascript to all pages

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Maverick-s-Mission
+
+This project is a website-in-progress for a friend of mine who has
+an animal sanctuary called Maverick's Mission. I tried to incorporate
+the same styling as other sanctuaries I found online that I liked. 
+I worked mainly on the Home and Meet the Animals pages with the Donate, 
+Contact, and FAQs pages only having the header, footer, and hero sections. 
+This was a fun first project that I look forward to building onto.  
+
+The features:
+1) Responsive menu with Javascript
+2) Transition animations to elements on hover
+3) Run PageSpeed Insights of 60 or better
--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-$(document).ready(function(){
+/* Nav Bar Hamburger Button*/
+const hamburgerButton = document.querySelector(".hamburger-button");
 
-    //hamburger Toggle
-    $('.humbarger').click(function(event){
-    $('.menu-list').slideToggle(500);
-    event.preventDefault();
-    
-    $('.menu-list li a').click(function(event) {
-        if ($(window).width() < 768) {
-          $('.menu-list').slideUp(500);
-          event.preventDefault(); 
-        }
-      });
-});
-
-});
-
-/* Homepage Nav Bar*/
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+function openMobileNav() {
+  const topNav = document.getElementById("myTopnav");
+  if (!topNav.classList.contains("responsive")) {
+    topNav.classList.add("responsive");
+  } else if (topNav.classList.contains("responsive")) {
+    topNav.classList.remove("responsive");
   }
+}
+hamburgerButton.addEventListener("click", openMobileNav);
